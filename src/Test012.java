be258775bfc4e141a375446e9f1ca14fb03c7934
rRain <rrainpath@gmail.com>
2021-09-23 21:02:15 +0800

    Java 17 Switch

--- src/Test012.java
+++ src/Test012.java
@@ -1,11 +1,22 @@
-
+import java.util.List;
+import java.util.Spliterator;
 
 public class Test012 {
     public static void main(String[] args) {
 
+        /*List<String> list = List.of("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
+        Spliterator<String> s1 = list.stream().spliterator();
 
+        s1.tryAdvance(System.out::println);
+        s1.tryAdvance(System.out::println);
 
+        Spliterator<String> s2 = s1.trySplit();
 
+        s1.tryAdvance(System.out::println);
+
+        s2.tryAdvance(System.out::println);
+
+        list.stream().reduce()*/
     }
 
 
