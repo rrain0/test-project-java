b7344c13d7f8ab585c355b7e2c031d323bc22b84
rRain <rrainpath@gmail.com>
2021-11-08 08:07:55 +0800

    Map Test

---
+++ src/MapTest.java
@@ -0,0 +1,59 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MapTest {
+    public static void main(String[] args) {
+
+
+        // порядок элементов в Map.of не сохраняется
+        System.out.println(Map.of("a",1, "b",2));
+        System.out.println(Map.of("b",2, "a",1));
+
+
+        Map<Token,Object> map = new HashMap<>();
+
+
+        map.put(new Token("aaa", 100L), "jjjj");
+        map.forEach((k,v)->{
+            System.out.println(k);
+            System.out.println(v);
+        });
+        map.put(new Token("aaa", 0L), "hhhh"); // сам ключ не обновится, только значение
+        map.forEach((k,v)->{
+            System.out.println(k);
+            System.out.println(v);
+        });
+        map.computeIfPresent(new Token("aaa", 100L), (t,v)->{
+            return null;
+        });
+        map.forEach((k,v)->{
+            System.out.println(k);
+            System.out.println(v);
+        });
+
+    }
+
+    private static record Token(
+        String token, Long time
+    ){
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Token token1 = (Token) o;
+            return Objects.equals(token, token1.token);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(token);
+        }
+    }
+}
--- src/Test013.java
+++ src/Test013.java
@@ -1,53 +1,5 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class Test013 {
     public static void main(String[] args) {
 
-        Map<Token,Object> map = new HashMap<>();
-
-
-        map.put(new Token("aaa", 100L), "jjjj");
-        map.forEach((k,v)->{
-            System.out.println(k);
-            System.out.println(v);
-        });
-        map.put(new Token("aaa", 0L), "jjjj");
-        map.forEach((k,v)->{
-            System.out.println(k);
-            System.out.println(v);
-        });
-        map.computeIfPresent(new Token("aaa", 100L), (t,v)->{
-            return null;
-        });
-        map.forEach((k,v)->{
-            System.out.println(k);
-            System.out.println(v);
-        });
-
-    }
-
-    private static record Token(
-        String token, Long time
-    ){
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Token token1 = (Token) o;
-            return Objects.equals(token, token1.token);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(token);
-        }
     }
 }
